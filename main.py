d781477f69f365ab028bb4bc6329c1e8a44daf28
Sandro Lutz <code@temparus.ch>
2019-09-03 15:54:59 +0200

    Update test client code

--- main.py
+++ main.py
@@ -6,17 +6,18 @@ RXD=4 # number of GPIO
 pi = pigpio.pi()
 
 if not pi.connected:
+    print('pigpio not connected!')
     exit(0)
 
 pigpio.exceptions = False # Ignore error if already set as bit bang read.
-
-handle = pi.file_open("/home/pi/Documents/bit_bang_output.txt",pigpio.FILE_WRITE) #assuming that the file /opt/pigpio/access (yes without extension) contains a line /home/pi/Domcuments/* w
 
 pi.bb_serial_read_open(RXD, 9600,9) # Set baud rate and number of data bits here. Reading 9 data bits will read the parity bit.
 
 pigpio.exceptions = True
 
 stop = time.time() + 10.0 # recording 10.0 seconds
+
+print('pigpio: start reading!')
 
 while time.time() < stop:
     (count, data) = pi.bb_serial_read(RXD)
@@ -26,5 +27,7 @@ while time.time() < stop:
         # pi.file_write(handle, data.hex())
 
 pi.bb_serial_read_close(RXD)
+
+print('pigpio: stopped reading!')
 
 pi.stop()
