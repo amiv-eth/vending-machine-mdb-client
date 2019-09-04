9502f5fd2235617c77e640cf6992840a6f35c98d
Sandro Lutz <code@temparus.ch>
2019-09-04 17:12:02 +0200

    More changes

--- MDBHandler.py
+++ MDBHandler.py
@@ -15,26 +15,26 @@ class MDBState(Enum):
     VEND = 5
 
 class MDBCommand():
-    RESET = b'\x00'
-    SETUP = b'\x01'
-    POLL = b'\x02'
-    VEND = b'\x03'
-    READER = b'\x04'
-    EXPANSION = b'\x07'
+    RESET =     0x00
+    SETUP =     0x01
+    POLL =      0x02
+    VEND =      0x03
+    READER =    0x04
+    EXPANSION = 0x07
 
 class MDBSubcommand():
-    SETUP_CONFIG_DATA = b'\x00'
-    SETUP_MAX_MIN_PRICES = b'\x01'
-    VEND_REQUEST = b'\x00'
-    VEND_CANCEL = b'\x01'
-    VEND_SUCCESS = b'\x02'
-    VEND_FAILURE = b'\x03'
-    VEND_SESSION_COMPLETE = b'\x04'
-    VEND_CASH_SALE = b'\x05'
-    READER_DISABLE = b'\x00'
-    READER_ENABLE = b'\x01'
-    READER_CANCEL = b'\x02'
-    EXPANSION_REQUEST_ID = b'\x00'
+    SETUP_CONFIG_DATA =     0x00
+    SETUP_MAX_MIN_PRICES =  0x01
+    VEND_REQUEST =          0x00
+    VEND_CANCEL =           0x01
+    VEND_SUCCESS =          0x02
+    VEND_FAILURE =          0x03
+    VEND_SESSION_COMPLETE = 0x04
+    VEND_CASH_SALE =        0x05
+    READER_DISABLE =        0x00
+    READER_ENABLE =         0x01
+    READER_CANCEL =         0x02
+    EXPANSION_REQUEST_ID =  0x00
 
 
 CommandToFrameLengthMapping = {
@@ -53,27 +53,6 @@ CommandToFrameLengthMapping = {
 
 
 class MDBHandler():
-    # Timeout in seconds
-    TIMEOUT = 12
-
-    # MDB Constants
-    # MDB_JUST_RESET = b'\x00'
-    MDB_POLL = b'\x12'
-    MDB_RESET = b'\x10\x10'
-    MDB_READER_ENABLE = b'\x14\x01'
-    MDB_OUT_OF_SEQUENCE = b'\x0B'
-    MDB_READER_CONFIG_RESPONSE = b'\x01\x01\x02\xF4\x01\x02\x02\x00'
-    MDB_EXT_FEATURES_RESPONSE = b'\x09\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-    MDB_VEND_REQUEST = b'\x13\x00'
-    MDB_VEND_SUCCESFUL = b'\x13\x02'
-    MDB_VEND_CANCEL = b'\x13\x01'
-    MDB_SESSION_COMPLETE = b'\x13\x04'
-    MDB_OPEN_SESSION = b'\x03\x05\x39'
-    MDB_CANCEL_REQUEST = b'\x04'
-    MDB_END_SESSION = b'\x07'
-    MDB_VEND_DENIED = b'\x06'
-    MDB_VEND_APPROVED = b'\x05\x00\x02'
-
     DISPLAY_WIDTH = 16
 
     def __init__(self, pi, rx_gpio, tx_gpio):
@@ -98,17 +77,17 @@ class MDBHandler():
             # self.handle_frame(frame)
 
     def session_open(self):
-        self.send_buffer.append(b'\x03\x05\x39')
+        self.send_buffer.append([0x03, 0x05, 0x39)
 
     def session_display_request(self, content):
         # will show the text for 6 seconds
-        self.send_buffer.append(b'\x02\x3C' + content)
+        self.send_buffer.append([0x02, 0x3C] + content)
 
     def session_cancel(self):
-        self.send_buffer.append(b'\x04')
+        self.send_buffer.append([0x04])
 
     def session_close(self):
-        self.send_buffer.append(b'\x07')
+        self.send_buffer.append([0x07])
 
     def reset(self):
         self.state = MDBState.RESET
@@ -122,17 +101,17 @@ class MDBHandler():
         self.pi.bb_serial_read_close(self.rx_gpio)
 
     def send_ack(self):
-        self.send(b'\x00')
+        self.send([0x00])
 
     def send_nack(self):
-        self.send(b'\xff')
+        self.send([0xff])
 
     def send_data(self, data):
         self.pi.wave_clear()
         frame = []
         checksum = 0
         for i in range(0, len(data)):
-            frame.append(bytes([data[i]]))
+            frame.append(data[i])
             frame.append(0) # set parity bit to zero
             checksum = (checksum + data[i]) % 256
         # add checksum with parity bit set
@@ -185,7 +164,7 @@ class MDBHandler():
             if self.has_pending_frame and len(self.frame_buffer) == self.frame_expected_length:
                 # TODO: verify checksum and handle received frame
                 self.has_pending_frame = False
-                if int.from_bytes(self.frame_buffer[len(self.frame_buffer)-1], byteorder="big") == self.frame_checksum:
+                if self.frame_buffer[len(self.frame_buffer)-1] == self.frame_checksum:
                     # A valid frame was received!
                     print('Received a valid frame! YAY!')
                     return self.frame_buffer
@@ -198,39 +177,39 @@ class MDBHandler():
     def print_frame(self, frame):
         print('New frame received! | ', end='')
         print('Address: ', end='')
-        print((frame[0] & 248).hex(), end=', ')
+        print((frame[0] & 0xf8).hex(), end=', ')
         print('Length: ', end='')
         print(len(frame), end=', ')
         print('Checksum: ', end='')
         print(frame[len(frame)-1].hex(), end=', ')
         print('Command: ', end='')
-        print((frame[0] & 7).hex(), end=', ')
+        print((frame[0] & 0x07).hex(), end=', ')
         if len(frame) > 2:
             print('Data: ', end='')
             for i in range(1, len(frame)):
                 print(frame[i].hex(), end='')
 
     def handle_frame(self, frame):
-        address = frame[0] & 248
+        address = frame[0] & 0xf8
         length = len(frame)
-        command = frame[0] & 7
+        command = frame[0] & 0x07
 
         # Only handle frames addressed to this device! Sniffing comes with revision 2!
-        if address != 16: # Address: 0x10
+        if address != 0x10: # Address: 0x10
             return
 
-        if command == self.MDB_POLL:
+        if command == MDBCommand.POLL:
             if self.state == MDBState.RESET:
-                self.send_data(b'\x00') # send JUST_RESET
+                self.send_data([0x00]) # send JUST_RESET
                 self.state == MDBState.DISABLED
                 return
             elif self.state != MDBState.DISABLED and len(self.send_buffer) > 0:
                 # send enqueued messages
                 data = self.send_buffer.pop(0)
                 self.send_data(data)
-                if data[0] == b'\x03':
+                if data[0] == 0x03:
                     self.state = MDBState.SESSION_IDLE
-                elif data[0] == b'\x04' or data[0] == b'\x07':
+                elif data[0] == 0x04 or data[0] == 0x07:
                     self.state = MDBState.ENABLED
             else:
                 self.send_ack()
@@ -245,19 +224,19 @@ class MDBHandler():
                     return
             elif frame[1] == MDBSubcommand.READER_CANCEL:
                 if self.state != MDBState.DISABLED:
-                    self.send_data(b'\x08')
+                    self.send_data([0x08])
                     # TODO: cancel current session!
                     self.state = MDBState.ENABLED
         elif command == MDBCommand.SETUP:
             if frame[1] == MDBSubcommand.SETUP_CONFIG_DATA:
-                self.send_data(b'\x01\x01\x02\xF4\x01\x02\x02\x02')
+                self.send_data([0x01,0x01,0x02,0xF4,0x01,0x02,0x02,0x02])
                 return
             elif frame[1] == MDBSubcommand.SETUP_MAX_MIN_PRICES:
                 self.send_ack()
                 return
         elif command == MDBCommand.EXPANSION:
             if frame[1] == MDBSubcommand.EXPANSION_REQUEST_ID:
-                self.send_data(b'\x09\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+                self.send_data([0x09,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00])
                 return
         elif command == MDBCommand.RESET:
             self.send_ack()
