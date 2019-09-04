d64b9fd25d626d8e522d935b4531ed77a5506288
Sandro Lutz <code@temparus.ch>
2019-09-04 16:26:59 +0200

    Moar debugging output

--- MDBHandler.py
+++ MDBHandler.py
@@ -168,6 +168,7 @@ class MDBHandler():
                 if self.has_pending_frame and len(self.frame_buffer) < self.frame_expected_length:
                     self.frame_buffer.append(data[pos])
                     frame_buffer_length = len(self.frame_buffer)
+                    print(frame_buffer_length)
                     if frame_buffer_length == 2:
                         commandFrameLength = CommandToFrameLengthMapping[self.frame_buffer[0] & 7]
                         if isinstance(commandFrameLength, int):
